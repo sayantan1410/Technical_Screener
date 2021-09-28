ae109a4da02f26a1c6150c5a06af3c9b5d078501
unknown <sayantan.sayanaldo@gmail.com>
2021-09-28 20:20:27 +0530

    Changes pushed

--- Data_pipeline.py
+++ Data_pipeline.py
@@ -1,16 +1,14 @@
 import requests 
 #from ta import add_all_ta_features
 import pandas as pd
-#from ta.momentum import RSIIndicator
-#API_key = 'HSJLSMXPLUPA99I5'
-#ts  = TimeSeries(key=API_key,output_format = 'pandas')
 def pipeline_intraday(ticker):
   params = {
-      'access_key' : '43893987de6db8c2ce031c6d3d9db375'
+      'access_key' : '0f6a05295cb99df83172b6be1676f7e7',
+      'limit' : '3000'
   }
-  response = requests.get(f'http://api.marketstack.com/v1/tickers/{ticker}/eod',params)
+  response = requests.get(f'http://api.marketstack.com/v1/tickers/{ticker}/intraday',params)
   response = response.json()
-  response = response['data']['eod']
+  response = response['data']['intraday']
   df1 = pd.DataFrame(response)
   df1.set_index(['date'],inplace=True)
   df1 = df1.iloc[:,0:5]
@@ -19,7 +17,9 @@ def pipeline_intraday(ticker):
   df1 = df1.rename(columns={"low":"Low"})
   df1 = df1.rename(columns={"close":"Close"})
   df1 = df1.rename(columns={"volume":"Volume"})
-  df1.index = pd.to_datetime(df1.index)
+  if (df1.isnull().values.any() == True):
+    df1.dropna(axis = 0,inplace=True)
   df1 = df1.sort_index(ascending=True)
+  df1.index = pd.to_datetime(df1.index)
   return df1
 # More functions for fetching daily data, monthly data and historical data to be updated soon. 
